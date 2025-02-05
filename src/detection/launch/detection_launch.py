82790b4a023957db99bff31a995bd98a3b4a2b6f
thaeron <thaeron@kth.se>
2025-02-05 16:40:50 +0100

    Detection launch file

--- src/detection/launch/detection_launch.py
+++ src/detection/launch/detection_launch.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python3
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource, AnyLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    frames_launch_file = os.path.join(
+        get_package_share_directory("robp_launch"), "launch", "frames_launch.xml"
+    )
+    camera_launch_file = os.path.join(
+        get_package_share_directory("robp_launch"), "launch", "rs_d435i_launch.py"
+    )
     return LaunchDescription([   
+        IncludeLaunchDescription(
+            launch_description_source=AnyLaunchDescriptionSource(frames_launch_file),
+        ),
+        IncludeLaunchDescription(
+            launch_description_source=PythonLaunchDescriptionSource(camera_launch_file),
+        ),
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=["0", "0", "0", "0", "0", "0", "1", "map", "base_link"],
+            output="screen",
+        ),
         Node(
             package='detection',
             executable='detection',
-            name='detection',
-            # output='screen',
+            name='robot_detection',
         ),
     ])
