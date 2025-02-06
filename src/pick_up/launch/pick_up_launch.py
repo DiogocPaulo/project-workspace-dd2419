f96bcd4da40046d7f17887f1d450013f1f78a718
thaeron <thaeron@kth.se>
2025-02-06 11:34:15 +0100

    changed arm launch file

--- src/pick_up/launch/pick_up_launch.py
+++ src/pick_up/launch/pick_up_launch.py
@@ -18,6 +18,13 @@ def generate_launch_description():
             name='servo_pos_subscriber',
             # output='screen',
         ),
+        Node(
+            package='micro_ros_agent',
+            executable='micro_ros_agent',
+            name='micro_ros_agent',
+            output='screen',
+            arguments=["serial", "--dev", "/dev/ttyUSB1", "-v6"] # Changed to 1
+        ),
     ])
 
 """Node(
