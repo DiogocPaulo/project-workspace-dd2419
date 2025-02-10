9224e7c2bda297d4c2a081582c66442678c26560
Gabriell <gaah@kth.se>
2025-02-10 13:04:20 +0100

    /scan listener, /ag_scan publisher

--- src/lidar/lidar/lidar.py
+++ src/lidar/lidar/lidar.py
@@ -1,5 +1,39 @@
-def main():
-    print('Hi from lidar.')
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import LaserScan
+
+class LidarRepublisher(Node):
+    def __init__(self):
+        super().__init__('lidar')
+        
+        self.subscriber = self.create_subscription(
+            LaserScan,
+            '/scan',
+            self.scan_callback,
+            10
+        )
+        
+        self.publisher = self.create_publisher(
+            LaserScan,
+            '/ag_scan',
+            10
+        )
+
+    def scan_callback(self, msg):
+        self.publisher.publish(msg)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = LidarRepublisher()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
