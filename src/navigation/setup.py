db4aa834ebeb8e732cab2c4ca03e84241fb185c3
thaeron <thaeron@kth.se>
2025-02-10 14:33:34 +0100

    testing navigation

--- src/navigation/setup.py
+++ src/navigation/setup.py
@@ -1,16 +1,19 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'navigation'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append((os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))))
+data_files.append((os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))))
+data_files.append(('share/' + package_name, ['package.xml']))
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='dev',
@@ -20,7 +23,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'navigation = navigation.navigation:main'
+            'navigation = navigation.navigation:main',
+            'pathing = navigation.pathing:main',
         ],
     },
 )
