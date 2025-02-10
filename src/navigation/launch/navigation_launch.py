01579280d7cefb7d3f0d007e2eea65481a5b621f
thaeron <thaeron@kth.se>
2025-02-10 14:33:34 +0100

    testing navigation

---
+++ src/navigation/launch/navigation_launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource, AnyLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="navigation",
+            executable="navigation",
+            name="robot_navigation",
+        ),
+        Node(
+            package="navigation",
+            executable="pathing",
+            name="robot_pathing",
+        ),
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=["0", "0", "0", "0", "0", "0", "1", "map", "odom"],
+            output="screen",
+        ),
+    ])
--- src/navigation/navigation/navigation.py
+++ src/navigation/navigation/navigation.py
@@ -10,6 +10,7 @@ from robp_interfaces.msg import Encoders
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped
 
+from robp_interfaces.msg import DutyCycles
 from tf2_ros import TransformBroadcaster
 from tf_transformations import quaternion_from_euler, euler_from_quaternion
 
@@ -30,8 +31,8 @@ class RobotState:
         self.velocity = 0.0
 
     def update_state(self, odom_msg):
-        self.x = odom_msg.pose.position.x
-        self.y = odom_msg.pose.position.y
+        self.x = odom_msg.poses[-1].pose.position.x
+        self.y = odom_msg.poses[-1].pose.position.y
 
         q = odom_msg.pose.orientation
         t0 = +2.0 * (q.w * q.z + q.x * q.y)
@@ -56,7 +57,7 @@ class TargetPath:
         self.old_nearest_point_index = None
 
     def search_target_index(self, state):
-        if not x_points or not y_points:
+        if not self.x_points or not self.y_points:
             # Checks if there exits a path
             return None, None
 
@@ -90,7 +91,7 @@ class TargetPath:
 
         return index, lookahead
 
-def pure_pursuit_control(self, state, target_path):
+def pure_pursuit_control(state, target_path):
     index, lookahead = target_path.search_target_index(state)
     
     # if previous_index >= index:
@@ -117,15 +118,10 @@ class Navigation(Node):
     def __init__(self):
         super().__init__("navigation")
 
-        self.lookahead_distance = 1
-        self.max_speed = 0.6
-        self.current_pose = None
-        self.pathing_points = []
-
         self.state = RobotState()
         self.target_path = TargetPath()
         self.previous_index = 0
-        self.stamp = 0
+        #self.stamp = None
 
         self.create_subscription(
                 Path,
@@ -134,7 +130,7 @@ class Navigation(Node):
                 10)
         self.create_subscription(
                 Path,
-                "current_path",
+                "custom_path",
                 self.path_callback,
                 10)
         self.motor_publisher = self.create_publisher(DutyCycles, "/motor/duty_cycles", 10)
@@ -144,33 +140,33 @@ class Navigation(Node):
 
     def odom_callback(self, msg: Path):
         self.state.update_state(msg)
-        self.stamp = msg.poses[-1].header.stamp
+        #self.stamp = msg.poses[-1].header.stamp
 
     def path_callback(self, msg: Path):
         self.target_path.update_path(msg)
 
-    def control_loop(self)
+    def control_loop(self):
         if not self.target_path.x_points:
             self.get_logger().info(f"Error - No target path")
             return
 
-        delta, self.previous_index = self.target_path.pure_pursuit_control(self.state, self.target_path, self.previous_index)
+        delta, self.previous_index = pure_pursuit_control(self.state, self.target_path)
+        if DEBUG: self.get_logger().info(f"Velocity: {self.state.velocity}, Steering Angle: {delta:.2f}, Target Index: {self.previous_index}")
 
-        left_wheel = self.state.velocity + delta
-        right_wheel = self.state.velocity - delta
+        # left_wheel = self.state.velocity + delta
+        # right_wheel = self.state.velocity - delta
+        #
+        # max_value = max(abs(left_wheel), abs(right_wheel))
+        #
+        # if max_value > 1:
+        #     left_wheel = left_wheel / max_value
+        #     right_wheel = right_wheel / max_value
+        #
+        # dutyCycles = DutyCycles()
+        # dutyCycles.header.frame_id = "base_link"
+        # dutyCycles.header.stamp = self.stamp
+        # self.motor_publisher.publish(dutyCycles)
 
-        max_value = max(abs(left), abs(right))
-
-        if max_value > 1:
-            left_wheel = left_wheel / max_value
-            right_wheel = right_wheel / max_value
-
-        dutyCycles = DutyCycles()
-        dutyCycles.header.frame_id = "base_link"
-        dutyCycles.header.stamp = self.stamp
-        self.motor_publisher.publish(dutyCycles)
-
-        if DEBUG: self.get_logger().info(f"Velocity: {self.state.velocity}, Steering Angle: {delta:.2f}, Target Index: {self.prev_index}")
 
 def main():
     rclpy.init()
