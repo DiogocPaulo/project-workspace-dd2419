8f937c5dd7e418f8e723c069077f9b2fd143d74e
thaeron <thaeron@kth.se>
2025-02-14 14:31:34 +0100

    separate launch file for phidgets and odometry

--- src/localisation/launch/localisation_launch.py
+++ src/localisation/launch/localisation_launch.py
@@ -1,3 +1,5 @@
+# Launches odometry and lidar (requires setup_launch to be run first)
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
@@ -8,42 +10,18 @@ import os
 
 from launch_ros.actions import Node
 
-# Launches odometry and lidar
 def generate_launch_description():
-    frames_launch_file = os.path.join(
-        get_package_share_directory("robp_launch"), "launch", "frames_launch.xml"
-    ) 
     lidar_launch_file = os.path.join(
         get_package_share_directory("robp_launch"), "launch", "lidar_launch.yaml"
     ) 
-    phidgets_launch_file = os.path.join(
-        get_package_share_directory("robp_launch"), "launch", "phidgets_launch.py"
-    ) 
     return LaunchDescription([
         IncludeLaunchDescription(
-            launch_description_source=PythonLaunchDescriptionSource(phidgets_launch_file),
+            launch_description_source=AnyLaunchDescriptionSource(lidar_launch_file),
         ),
-        # IncludeLaunchDescription(
-        #     launch_description_source=AnyLaunchDescriptionSource(frames_launch_file),
-        # ),
-        # IncludeLaunchDescription(
-        #     launch_description_source=AnyLaunchDescriptionSource(lidar_launch_file),
-        # ),
         Node(
             package="localisation",
             executable="odometry",
             name="robot_odometry",
-        ),
-        Node(
-            package="localisation",
-            executable="localisation",
-            name="robot_localisation",
-        ),
-        Node(
-            package="tf2_ros",
-            executable="static_transform_publisher",
-            arguments=["0", "0", "0", "0", "0", "0", "1", "map", "odom"],
-            output="screen",
         ),
     ])
 
