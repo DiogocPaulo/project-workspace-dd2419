c404c599c9882c4f92be0cea5d0ef7949c83f499
thaeron <thaeron@kth.se>
2025-02-17 15:42:54 +0100

    Started dynamic pickup

--- src/pick_up/pick_up/move_servos_publisher.py
+++ src/pick_up/pick_up/move_servos_publisher.py
@@ -1,95 +1,178 @@
-"""import rclpy
-from rclpy.node import Node
-
-from std_msgs.msg import String
-from std_msgs.msg import Int16MultiArray, MultiArrayDimension, MultiArrayLayout
-
-import time
-
-
-class MinimalPublisher(Node):
-
-    def __init__(self):
-        super().__init__('move_servos_publisher')
-        self.publisher_ = self.create_publisher(Int16MultiArray, '/multi_servo_cmd_sub', 10)
-        timer_period = 5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
-
-    def timer_callback(self):
-        # pick up stuff
-        # data_sets = [[2000,12000,20000,3000,-1,3000,500,500,500,500,500,500],
-        #             [16000,-1,-1,-1,-1,-1,500,500,500,500,500,500],
-        #             [16000,12000,12000,12000,12000,12000,500,500,500,500,500,500],
-        #             [12000,12000,12000,12000,12000,12000,500,500,500,500,500,500]]
-
-        # msg = Int64MultiArray()
-
-        # msg.data = data_sets[self.i]
-        # self.publisher_.publish(msg)
-
-        # self.i += 1
-        # if self.i == 4:
-        #     self.i = 0
-
-        
-        # random movement
-        data_sets = [[5000,13000,14000,10000,16000,8000,500,500,500,500,500,500],
-                    [12000,12000,12000,12000,12000,12000,500,500,500,500,500,500]]
-        msg = Int16MultiArray()
-        msg.layout = MultiArrayLayout(dim=[MultiArrayDimension(label="", size=0, stride=0)], data_offset=0)
-        msg.data = data_sets[self.i]
-        self.publisher_.publish(msg)
-
-        self.i += 1
-        if self.i == 2:
-            self.i = 0
-
-        
-        # self.get_logger().info('Publishing: "%s"' % str(msg.data))
-        # self.i += 1   
-
-def main():
-    rclpy.init()
-    node = MinimalPublisher()
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-
-    rclpy.shutdown()
-
-
-if __name__ == '__main__':
-    main()
-"""
-
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16MultiArray, MultiArrayLayout, MultiArrayDimension
 
+from rclpy.action import ActionServer
+from geometry_msgs.msg import Point
+from tf2_ros import TransformException
+from tf2_geometry_msgs import do_transform_point
+# from pick_up.action import PickupAction
+
+import math
+import tf2_ros
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
 class MultiServoPublisher(Node):
     def __init__(self):
         super().__init__("multi_servo_publisher")
-        self.timer = self.create_timer(5.0, self.publish_pose)
+        # self.timer = self.create_timer(5.0, self.publish_pose)
         self.publisher = self.create_publisher(Int16MultiArray, "/multi_servo_cmd_sub", 10)
         self.i = 0
 
-    def publish_pose(self):
-        self.get_logger().info(f"Published servo command message")
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer,self)
+        
+
+        self.publish_pose()
+        # self.server = ActionServer(self,PickupAction,'PickupCube',self.pickup_callback)
+
+    def publish_pose(self): #Test function (not the actual function)
+        self.get_logger().info(f"Lets go!")
         msg = Int16MultiArray()
         msg.layout = MultiArrayLayout(dim=[MultiArrayDimension(label="", size=12, stride=12)], data_offset=0)
-        
-        data_sets = [[3000,12000,12000,12000,12000,12000,1000,1000,1000,1000,1000,1000],
-                    [3000,12000,8000,20000,6700,12000,1000,1000,1000,1000,1000,1000],
-                    [12000,12000,8000,20000,6700,12000,1000,1000,1000,1000,1000,1000],
-                    [12000,12000,12000,12000,12000,12000,1000,1000,1000,1000,1000,1000]]
-        msg.data = data_sets[self.i]
+        move_time = 1000 #arm speed (milliseconds)
+
+        # pose publish
+        # If not already in the base pose, go to it:
+        pose = [3000,12000,12000,12000,12000,12000,move_time,move_time,move_time,move_time,move_time,move_time]
+        msg.data = pose
         self.publisher.publish(msg)
-        self.get_logger().info(f"Published servo command message")
-        self.i += 1
-        if self.i == 4:
-            self.i = 0
+
+        # TODO: Get transform between arm base and map (SOLVED)
+        tf_future = self.tfBuffer.wait_for_transform_async(
+            target_frame = 'arm_base',
+            source_frame = 'map',
+            time = 0 # Get latest transform instead of timestamped, since we want to pickup when the robot is standing still
+        )
+
+        rclpy.spin_until_future_complete(self,tf_future, timeout_sec=1)
+
+        try:
+            t = self.tfBuffer.lookup_transform(
+                'arm_base',
+                'map',
+                0)
+        except TransformException as ex:
+            self.get_logger().info(
+                f'Could not transform map to arm_base: {ex}'
+            )
+
+        rclpy.sleep(1.5) #Give arm time to do its thing
+
+        # TODO: Get position of object in map frame (SOLVED)
+
+        
+        world_position = Point()
+        world_position.x = 0.5
+        world_position.y = 0.5
+        world_position.z = 0.1
+
+
+        self.get_logger().info(f"Received goal: Pickup object at position {world_position} in map frame")
+
+        # TODO: Transform the object into the arm base frame (SOLVED)
+        position = do_transform_point(world_position,t)
+
+        # TODO: Calculate the arm parameters to "hawk" over the object's position
+        # First calculate the base rotation
+        base_rotation_angle = math.atan2(position)
+
+
+        # TODO: transform angle from radians to arm parameters
+
+        # placeholder
+        pose = [12000,12000,8000,20000,6700,6000,move_time,move_time,move_time,move_time,move_time,move_time]
+        msg.data = pose
+        self.publisher.publish(msg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def pickup_callback(self,goal_handle):
+    #     msg = Int16MultiArray()
+    #     msg.layout = MultiArrayLayout(dim=[MultiArrayDimension(label="", size=12, stride=12)], data_offset=0)
+    #     move_time = 1000 #arm speed (milliseconds)
+
+    #     # pose publish
+    #     # If not already in the base pose, go to it:
+    #     pose = [3000,12000,12000,12000,12000,12000,move_time,move_time,move_time,move_time,move_time,move_time]
+    #     msg.data = pose
+    #     self.publisher.publish(msg)
+
+    #     # TODO: Get transform between arm base and map (SOLVED)
+    #     tf_future = self.tf_buffer.wait_for_transform_async(
+    #         target_frame = 'arm_base',
+    #         source_frame = 'map',
+    #         time = 0 # Get latest transform instead of timestamped, since we want to pickup when the robot is standing still
+    #     )
+
+    #     rclpy.spin_until_future_complete(self,tf_future, timeout_sec=1)
+
+    #     try:
+    #         t = self.tf_buffer.lookup_transform(
+    #             'arm_base',
+    #             'map',
+    #             0)
+    #     except TransformException as ex:
+    #         self.get_logger().info(
+    #             f'Could not transform map to arm_base: {ex}'
+    #         )
+
+    #     rclpy.sleep(1.5) #Give arm time to do its thing
+
+    #     # TODO: Get position of object in map frame (SOLVED)
+    #     world_position = goal_handle.request.position
+    #     self.get_logger().info(f"Received goal: Pickup object at position {world_position} in map frame")
+
+    #     # TODO: Transform the object into the arm base frame (SOLVED)
+    #     position = do_transform_point(world_position,t)
+
+    #     # TODO: Calculate the arm parameters to "hawk" over the object's position
+    #     # First calculate the base rotation
+    #     base_rotation_angle = math.atan2(position)
+
+
+    #     # TODO: transform angle from radians to arm parameters
+
+    #     # TODO: Publish said pose to the arm
+    #     # placeholder
+    #     pose = [12000,12000,8000,20000,6700,base_rotation_angle,move_time,move_time,move_time,move_time,move_time,move_time]
+    #     msg.data = pose
+    #     self.publisher.publish(msg)
+
+    #     # TODO: See and confirm the object is in the right position (using the arm camera)
+
+    #     # TODO: If yes (the object is there and in correct position) then calculate and publish a movement to move into pre pickup pose
+    #     # TODO: Close gripper
+    #     # TODO: Move arm back to base position
+    #     # TODO: Confirm object is in gripper using arm camera
+
+    #     # TODO: If no (the object is not there or in incorrect position)
+    #     # TODO: Go back to base position
+    #     # TODO: If found recalculate the new postion, move to it and try again
+
+    #     # TODO: Done
+
+
+        
 
 
 def main():
--- src/pick_up/setup.py
+++ src/pick_up/setup.py
@@ -9,6 +9,7 @@ data_files.append((os.path.join('share', package_name, 'launch'), glob(os.path.j
 data_files.append((os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))))
 data_files.append(('share/' + package_name, ['package.xml']))
 data_files.append((os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))))
+data_files.append((os.path.join('share', package_name, 'action'), glob(os.path.join('action', '*.action'))))
 
 setup(
     name=package_name,
