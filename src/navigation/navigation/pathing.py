44303477716af20810027d60ca352aa7d2a47d8f
thaeron <thaeron@kth.se>
2025-02-25 18:36:05 +0100

    more test

--- src/navigation/navigation/pathing.py
+++ src/navigation/navigation/pathing.py
@@ -26,7 +26,7 @@ class Pathing(Node):
 
         # Parameters
         self.start_point = (0, 0)
-        self.end_point = (1.8, -0.5)
+        self.end_point = (0, 0.5)
         self.amplitude = 0.5
         self.cycles = 1.0
         self.map_grid = None
