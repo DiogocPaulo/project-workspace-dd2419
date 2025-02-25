9bee48097e5b9ec4e34398ba8d59686284e50e2f
eliaswet <eliaswet@kth.se>
2025-02-25 16:44:50 +0100

    Fixed IK for simulation

--- src/pick_up/pick_up/move_servos_publisher.py
+++ src/pick_up/pick_up/move_servos_publisher.py
@@ -39,13 +39,14 @@ class MultiServoPublisher(Node):
         # self.server = ActionServer(self,PickupAction,'PickupCube',self.pickup_callback)
 
         self.position = Point()
-        self.position.x = 0.15
-        self.position.y = 0.0
-        self.position.z = 0.0
+        self.position.x = 0.0
+        self.position.y = 0.15   
+        self.position.z = -0.22  
+        self.desired_grip_angle = math.pi/2
 
         self.publisher_marker = self.create_publisher(Marker, '/visualization_marker', 10)
 
-        self.timer = self.create_timer(1.0, self.publish_object_marker)
+        self.timer = self.create_timer(0.1, self.publish_object_marker)
                 
 
         self.clock = self.get_clock()
@@ -131,16 +132,16 @@ class MultiServoPublisher(Node):
         self.get_logger().info(f"Simulating!")
 
         
-        joint_names = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5']
+        joint_names = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5','r_joint']
 
         move_time = 2000 #arm speed (milliseconds)
 
         # pose publish
         # If not already in the base pose, go to it:
         
-        self.publish_JointStates(joint_names,[0.0,0.0,0.0,0.0,0.0])
+        self.publish_JointStates(joint_names,[0.0,0.0,0.0,0.0,0.0,0.0])
 
-        self.clock.sleep_for(rclpy.duration.Duration(seconds=5))
+        self.clock.sleep_for(rclpy.duration.Duration(seconds=2))
 
 
 
@@ -151,15 +152,15 @@ class MultiServoPublisher(Node):
         distance = self.distance_calc(0,0,self.position.x,self.position.y)
         self.get_logger().info(f"Distance: {distance}!")
 
-        alpha,beta = self.CalcKinematics(distance,self.position.z-0.065)
+        alpha,beta,charlie = self.CalcKinematics(distance,self.position.z,self.desired_grip_angle)
 
         self.get_logger().info(f"Alpha: {alpha} Beta: {beta}")
 
-        self.publish_JointStates(joint_names,[base_rotation_angle,(1.570-alpha),(2.090-beta),0.0,0.0])
+        self.publish_JointStates(joint_names,[base_rotation_angle,(alpha),(beta),charlie,0.0,0.0])
 
-        # self.clock.sleep_for(rclpy.duration.Duration(seconds=5))
+        self.clock.sleep_for(rclpy.duration.Duration(seconds=5))
 
-        # self.publish_JointStates(joint_names,[0,0,0,0.0,0.0])
+        self.publish_JointStates(joint_names,[0,0,0,0.0,0.0,0.0])
 
 
         self.get_logger().info("Done!")
@@ -247,27 +248,30 @@ class MultiServoPublisher(Node):
     #     # TODO: Done
 
 
-    def CalcKinematics(self,x,y): #Servos 5 & 4
+    def CalcKinematics(self,x,y,desired_grip_angle): #Servos 5 & 4
         self.get_logger().info(f'Position: {x},{y}')
         l1 = 0.101
         l2 = 0.095
+        l4 = 0.168
 
-        l3 = (x**2 + y**2)
+        #Calc gripper
+        py = math.sin(desired_grip_angle)*l4 + y
+        px = x - math.cos(desired_grip_angle)*l4
 
-        c2 = (l1**2 + l2**2 - l3) / (2*l1*l2)
+        # Calc joint 1-3
+        l3 = math.sqrt(px**2 + py**2)
+
+        c2 = (l1**2 + l2**2 - l3**2) / (2 * l1 * l2)
         v2 = math.acos(c2)
 
-        c1 = (l1**2 + l3 - l2**2) / (2*l1*math.sqrt(l3))
+        c1 = (l1**2 + l3**2 - l2**2) / (2*l1*l3)
         v1 = math.acos(c1)
 
-        # alpha = math.atan2(y,x)
+        base_angle = math.atan2(py,px)
 
-        # beta = math.acos((x**2 + y**2 + l1**2 - l2**2)/(2*l1*math.sqrt(x**2 + y**2)))
+        v3 = (math.pi/2 - base_angle) + (math.pi - v1 - v2) + (math.pi/2 - desired_grip_angle) - math.pi
 
-        # v1 = alpha - beta 
-        
-
-        return v1,v2
+        return (math.pi/2) - v1 - base_angle,math.pi - v2, -v3
     
     def publish_JointStates(self, names, states):
         self.get_logger().info("*** Publishing!***")
@@ -291,7 +295,7 @@ class MultiServoPublisher(Node):
     def publish_object_marker(self):
         # Create the marker
         marker = Marker()
-        marker.header.frame_id = "xarm_base_link"  # This is the robot's base frame
+        marker.header.frame_id = "base_link"  # This is the robot's base frame
         marker.header.stamp = self.get_clock().now().to_msg()
         marker.ns = "object_namespace"
         marker.id = 0
@@ -299,7 +303,7 @@ class MultiServoPublisher(Node):
         marker.action = Marker.ADD
         marker.pose.position.x = self.position.x  # Position of the object (relative to the base)
         marker.pose.position.y = self.position.y
-        marker.pose.position.z = self.position.z + 0.065
+        marker.pose.position.z = self.position.z+0.065
         marker.scale.x = 0.05  # Scale of the sphere (radius)
         marker.scale.y = 0.05
         marker.scale.z = 0.05
