098c0254ffc740857d5f632618e18d4f6aff936a
thaeron <thaeron@kth.se>
2025-02-28 18:04:01 +0100

    Implemented real world arm controll. Now only needs a little bit of fine tuning and integration with other components

--- src/pick_up/pick_up/move_servos_publisher.py
+++ src/pick_up/pick_up/move_servos_publisher.py
@@ -40,10 +40,10 @@ class MultiServoPublisher(Node):
         # self.server = ActionServer(self,PickupAction,'PickupCube',self.pickup_callback)
 
         self.position = Point()
-        self.position.x = 0.13
-        self.position.y = 0.13   
-        self.position.z = -0.2
-        self.desired_grip_angle = math.pi/2
+        self.position.x = 0.15
+        self.position.y = 0.15
+        self.position.z = -0.16
+        self.desired_grip_angle = math.pi/3
 
         self.publisher_marker = self.create_publisher(Marker, '/visualization_marker', 10)
 
@@ -51,6 +51,11 @@ class MultiServoPublisher(Node):
                 
 
         self.publish_pose_sim()
+
+        self.clock.sleep_for(rclpy.duration.Duration(seconds=5))
+
+
+        self.publish_pose()
 
     def publish_pose(self): #Test function (not the actual function)
         self.get_logger().info(f"Applying to the real world")
@@ -71,24 +76,24 @@ class MultiServoPublisher(Node):
 
 
         # TODO: Get transform between arm base and map (SOLVED)
-        tf_future = self.tfBuffer.wait_for_transform_async(
-            target_frame = 'arm_base',
-            source_frame = 'map',
-            time = zero_time # Get latest transform instead of timestamped, since we want to pickup when the robot is standing still
-        )
+        # tf_future = self.tfBuffer.wait_for_transform_async(
+        #     target_frame = 'arm_base',
+        #     source_frame = 'map',
+        #     time = zero_time # Get latest transform instead of timestamped, since we want to pickup when the robot is standing still
+        # )
 
-        rclpy.spin_until_future_complete(self,tf_future, timeout_sec=1)
+        # rclpy.spin_until_future_complete(self,tf_future, timeout_sec=1)
 
-        try:
-            t = self.tfBuffer.lookup_transform(
-                'arm_base',
-                'map',
-                zero_time
-        )
-        except TransformException as ex:
-            self.get_logger().info(
-                f'Could not transform map to arm_base: {ex}'
-            )
+        # try:
+        #     t = self.tfBuffer.lookup_transform(
+        #         'arm_base',
+        #         'map',
+        #         zero_time
+        # )
+        # except TransformException as ex:
+        #     self.get_logger().info(
+        #         f'Could not transform map to arm_base: {ex}'
+        #     )
 
         self.clock.sleep_for(rclpy.duration.Duration(seconds=2)) #Give arm time to do its thing
 
@@ -96,32 +101,57 @@ class MultiServoPublisher(Node):
 
         
         world_position = Point()
-        world_position.x = 0.13
-        world_position.y = 0.13
-        world_position.z = -0.05
+        world_position.x = 0.2
+        world_position.y = 0.2
+        world_position.z = 0
 
 
         self.get_logger().info(f"Received goal: Pickup object at position {world_position} in map frame")
 
         # TODO: Transform the object into the arm base frame (SOLVED)
-        position = do_transform_point(world_position,t)
+        # position = do_transform_point(world_position,t)
+        position = world_position
+
 
         # TODO: Calculate the arm parameters to "hawk" over the object's position
 
         # perform IK
-        base,v1,v2,v3 = CalcKinematics(position.x,position.y,position.z,math.pi/2)
+        base_arm,v1_arm,v2_arm,v3_arm = self.FindKinematics(self.position.x,self.position.y,self.position.z)
 
-        #Convert angles to robot arm
-        base = 12000 + int(math.degrees(base)*10)
-        v1 = 12000 + int(math.degrees(v1)*10)
-        v2 = 12000 + int(math.degrees(v2)*10)
-        v3 = 12000 + int(math.degrees(v3)*10)
+        # #Convert angles to robot arm
+        # base_arm = 12000 - int(math.degrees(base)*100)
+        # v1_arm = 12000 - int(math.degrees(v1)*100)
+        # v2_arm = 12000 + int(math.degrees(v2)*100)
+        # v3_arm = 12000 - int(math.degrees(v3)*100)
 
-        self.get_logger().info(f"Received parameter: base={base} servo5={v1} servo4={v1}")
+        # if(base_arm < 0 or base_arm > 24000): return
+        # if(v1_arm < 6000 or v1_arm > 18000): return
+        # if(v2_arm < 3000 or v2_arm > 21000): return
+        # if(v3_arm < 3000 or v3_arm > 21000): return
 
-        self.clock.sleep_for(rclpy.duration.Duration(seconds=10))
+        self.get_logger().info(f"Received parameter: base={base_arm} servo5={v1_arm} servo4={v2_arm} servo3={v3_arm}")
+
+        self.clock.sleep_for(rclpy.duration.Duration(seconds=1))
         
-        pose = [3000,12000,12000,12000,12000,base,move_time,move_time,move_time,move_time,move_time,move_time]
+        pose = [3000,12000,v3_arm,v2_arm,v1_arm,base_arm,move_time,move_time,move_time,move_time,move_time,move_time]
+        msg.data = pose
+        self.publisher.publish(msg)
+
+        self.clock.sleep_for(rclpy.duration.Duration(seconds=2))
+        
+        pose = [12000,12000,v3_arm,v2_arm,v1_arm,base_arm,move_time,move_time,move_time,move_time,move_time,move_time]
+        msg.data = pose
+        self.publisher.publish(msg)
+
+        self.clock.sleep_for(rclpy.duration.Duration(seconds=5))
+
+        pose = [12000,12000,12000,12000,12000,12000,move_time,move_time,move_time,move_time,move_time,move_time]
+        msg.data = pose
+        self.publisher.publish(msg)
+
+        self.clock.sleep_for(rclpy.duration.Duration(seconds=5))
+
+        pose = [3000,12000,12000,12000,12000,12000,move_time,move_time,move_time,move_time,move_time,move_time]
         msg.data = pose
         self.publisher.publish(msg)
 
@@ -153,15 +183,15 @@ class MultiServoPublisher(Node):
         
         base,v1,v2,v3 = self.CalcKinematics(self.position.x,self.position.y,self.position.z,self.desired_grip_angle)
 
-        #Convert angles to robot arm
-        base_arm = 12000 - int(math.degrees(base)*100)
-        v1_arm = 12000 - int(math.degrees(v1)*100)
-        v2_arm = 12000 - int(math.degrees(v2)*100)
-        v3_arm = 12000 - int(math.degrees(v3)*100)
+        base_arm,v1_arm,v2_arm,v3_arm = self.FindKinematics2(self.position.x,self.position.y,self.position.z)
+
+
 
         self.get_logger().info(f"Received parameter: base={base_arm} servo5={v1_arm} servo4={v2_arm} servo3={v3_arm}")
 
-        self.publish_JointStates(joint_names,[base,v1,v2,v3,0.0,0.0])
+        
+        
+        self.publish_JointStates(joint_names,[base_arm,v1_arm,v2_arm,v3_arm,0.0,0.0])
         self.clock.sleep_for(rclpy.duration.Duration(seconds=5))
 
 
@@ -255,32 +285,90 @@ class MultiServoPublisher(Node):
 
 
     def CalcKinematics(self,x,y,z,desired_grip_angle): #Servos 5 & 4
-        self.get_logger().info(f'Position: {x},{y}')
+        # self.get_logger().info(f'Position: {x},{y}')
         l1 = 0.101
         l2 = 0.095
         l4 = 0.168
 
-        base_rotation_angle = (math.atan2(y,x))
-        d = self.distance_calc(0,0,x,y)
+        try:
+            base_rotation_angle = (math.atan2(y,x))
+            d = self.distance_calc(0,0,x,y)
 
-        #Calc gripper
-        py = math.sin(desired_grip_angle)*l4 + z
-        px = d - math.cos(desired_grip_angle)*l4
+            #Calc gripper
+            py = math.sin(desired_grip_angle)*l4 + z
+            px = d - math.cos(desired_grip_angle)*l4
 
-        # Calc joint 1-3
-        l3 = math.sqrt(px**2 + py**2)
+            # Calc joint 1-3
+            l3 = math.sqrt(px**2 + py**2)
 
-        c2 = (l1**2 + l2**2 - l3**2) / (2 * l1 * l2)
-        v2 = math.acos(c2)
+            c2 = (l1**2 + l2**2 - l3**2) / (2 * l1 * l2)
+            v2 = math.acos(c2)
 
-        c1 = (l1**2 + l3**2 - l2**2) / (2*l1*l3)
-        v1 = math.acos(c1)
+            c1 = (l1**2 + l3**2 - l2**2) / (2*l1*l3)
+            v1 = math.acos(c1)
 
-        base_angle = math.atan2(py,px)
+            base_angle = math.atan2(py,px)
 
-        v3 = (math.pi/2 - base_angle) + (math.pi - v1 - v2) + (math.pi/2 - desired_grip_angle) - math.pi
+            v3 = (math.pi/2 - base_angle) + (math.pi - v1 - v2) + (math.pi/2 - desired_grip_angle) - math.pi
 
-        return base_rotation_angle,(math.pi/2) - v1 - base_angle,math.pi - v2, -v3
+            return base_rotation_angle,(math.pi/2) - v1 - base_angle,math.pi - v2, -v3
+        except ValueError as e:
+            return -100,-100,-100,-100
+
+    def FindKinematics(self,x,y,z):
+
+        offset = 500
+
+        self.get_logger().info(f"OFFSET = {offset}")
+
+        for angle in range(0 + offset,9000 - offset):
+            angle = math.radians(angle/100)
+
+            base,v1,v2,v3 = self.CalcKinematics(x,y,z,angle)
+            base_arm = 12000 - int(math.degrees(base)*100)
+            v1_arm = 12000 - int(math.degrees(v1)*100)
+            v2_arm = 12000 + int(math.degrees(v2)*100)
+            v3_arm = 12000 - int(math.degrees(v3)*100)
+
+            if(base_arm < (0 + offset) or base_arm > (24000 - offset)): continue
+            if(v1_arm < (6000 + offset) or v1_arm > (18000 - offset)): continue
+            if(v2_arm < (3000 + offset) or v2_arm > (21000 - offset)): continue
+            if(v3_arm < (3000 + offset) or v3_arm > (21000 - offset)): continue
+
+            self.get_logger().info("Configuration has been found!")
+            return base_arm,v1_arm,v2_arm,v3_arm
+
+
+        self.get_logger().info("No configuration found!")
+        return 12000,12000,12000,12000
+
+    def FindKinematics2(self,x,y,z):
+
+        offset = 500
+
+        self.get_logger().info(f"OFFSET = {offset}")
+
+        for angle in range(0 + offset,9000 - offset):
+            angle = math.radians(angle/100)
+
+            base,v1,v2,v3 = self.CalcKinematics(x,y,z,angle)
+            base_arm = 12000 - int(math.degrees(base)*100)
+            v1_arm = 12000 - int(math.degrees(v1)*100)
+            v2_arm = 12000 + int(math.degrees(v2)*100)
+            v3_arm = 12000 - int(math.degrees(v3)*100)
+
+            if(base_arm < (0 + offset) or base_arm > (24000 - offset)): continue
+            if(v1_arm < (6000 + offset) or v1_arm > (18000 - offset)): continue
+            if(v2_arm < (3000 + offset) or v2_arm > (21000 - offset)): continue
+            if(v3_arm < (3000 + offset) or v3_arm > (21000 - offset)): continue
+
+            self.get_logger().info("Configuration has been found!")
+            return base,v1,v2,v3
+
+        self.get_logger().info("No configuration found!")
+        return 12000,12000,12000,12000
+
+
     
     def publish_JointStates(self, names, states):
         self.get_logger().info("***Publishing!***")
