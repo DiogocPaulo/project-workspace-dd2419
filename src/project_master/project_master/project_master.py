4aafe978af0d4d666dbdc3df20133f1f00e500e4
thaeron <thaeron@kth.se>
2025-03-03 19:26:37 +0100

    better trigger callback

--- src/project_master/project_master/project_master.py
+++ src/project_master/project_master/project_master.py
@@ -8,17 +8,34 @@ class ProjectMaster(Node):
         super().__init__("project_master")
 
         self.point_client = self.create_client(GoToPoint, "/navigation_point")
-        self.reached_destination_service = self.create_service(Trigger, "/reached_destination", self.reached_destination)
+        self.reached_destination_service = self.create_service(Trigger, "/reached_destination", self.reached_destination_callback)
         while not self.point_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().debug("GoToPoint service not yet avaliable, waiting ...")
 
-        self.send_end_point(-1.5, 0.5, 1.5)
+        self.send_end_point(-1.5, 0.5, 0)
 
-    def reached_destination(self, request, response):
-        self.send_end_point(1.5, 0.5, 1.5)
+    def reached_destination_callback(self, request, response):
+        self.get_logger().info("Master - Reached destination")
 
-        response.success = True
-        response.message = "Received reached destination trigger"
+        # Send new end point
+        go_to_point_request = GoToPoint.Request()
+        go_to_point_request.x = 1.5
+        go_to_point_request.y = 0.5
+        go_to_point_request.yaw = 1.5
+        future = self.point_client.call_async(go_to_point_request)
+
+        rclpy.spin_until_future_complete(self, future)
+
+        if future is not None:
+            go_to_point_response = future.result()
+            self.get_logger().info(f"GoToPoint response: {go_to_point_response.success}, {go_to_point_response.message}")
+            response.success = go_to_point_response.success
+            response.message = go_to_point_response.message
+        else:
+            self.get_logger().warn("Service call failed")
+            response.success = False
+            response.message = "Failed to call GoToPoint service"
+
         return response
 
     def send_end_point(self, x, y, yaw):
