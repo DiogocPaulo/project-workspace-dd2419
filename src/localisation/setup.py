03dc6c12e128ed5100ccb7d6d7cded62ebe91a58
thaeron <thaeron@kth.se>
2025-03-17 13:45:06 +0100

    readded odometry

--- src/localisation/setup.py
+++ src/localisation/setup.py
@@ -24,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'localisation = localisation.localisation:main',
+            'odometry = localisation.odometry:main',
         ],
     },
 )
