3e68670eb6bf247bd3c47bb7db9f43884adbaa7b
thaeron <thaeron@kth.se>
2025-03-17 13:41:17 +0100

    added launch imports

--- src/localisation/launch/localisation_launch.py
+++ src/localisation/launch/localisation_launch.py
@@ -1,7 +1,12 @@
 # Launches odometry node (requires setup_launch to be run first)
 
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource, AnyLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
     frames_launch_file = os.path.join(
