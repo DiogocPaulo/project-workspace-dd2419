470b9c5d02c90dc03fdc4823fe51ae93aa561f90
thaeron <thaeron@kth.se>
2025-03-17 13:42:36 +0100

    added more launch imports

--- src/localisation/launch/localisation_launch.py
+++ src/localisation/launch/localisation_launch.py
@@ -8,6 +8,8 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource, Any
 from ament_index_python.packages import get_package_share_directory
 import os
 
+from launch_ros.actions import Node
+
 def generate_launch_description():
     frames_launch_file = os.path.join(
         get_package_share_directory("robp_launch"), "launch", "frames_launch.xml"
