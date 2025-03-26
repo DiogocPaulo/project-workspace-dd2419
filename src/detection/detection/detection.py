f7ecd96d719782beb5c7140e8e71a7a308f975c1
thaeron <thaeron@kth.se>
2025-03-26 13:23:33 +0100

    removed brown cube detection

--- src/detection/detection/detection.py
+++ src/detection/detection/detection.py
@@ -39,7 +39,7 @@ class ExamineImage(Node):
         self.get_logger().info(f"Init detection")
 
         self.tfBuffer = tf2_ros.Buffer()
-        self.listener = tf2_ros.TransformListener(self.tfBuffer, self)
+        self.listener = tf2_ros.TransformListener(self.tfBuffer, self, spin_thread=True)
 
         self.object_list_broadcaster = tf2_ros.TransformBroadcaster(self)
 
@@ -204,7 +204,6 @@ class ExamineImage(Node):
         lower_green1, upper_green1 = np.array([81, 100, 44]), np.array([84, 255, 105])
         lower_green2, upper_green2 = np.array([73, 210, 90]), np.array([74, 240, 120])
         lower_blue, upper_blue = np.array([99, 254, 75]), np.array([99, 255, 80])
-        lower_brown, upper_brown = np.array([15, 68, 137]), np.array([17, 76, 134])
 
         # Iterate over each cluster
         for cluster_label in unique_labels:
@@ -225,13 +224,11 @@ class ExamineImage(Node):
             green_mask = (hsv_colors[:, 0] >= lower_green1[0]) & (hsv_colors[:, 0] <= upper_green1[0]) | \
                          ((hsv_colors[:, 0] >= lower_green2[0]) & (hsv_colors[:, 0] <= upper_green2[0]))
             blue_mask = (hsv_colors[:, 0] >= lower_blue[0]) & (hsv_colors[:, 0] <= upper_blue[0])
-            brown_mask = (hsv_colors[:, 0] >= lower_brown[0]) & (hsv_colors[:, 0] <= upper_brown[0])
 
             # Apply masks for the current cluster
             red_points = cluster_points[red_mask]
             green_points = cluster_points[green_mask]
             blue_points = cluster_points[blue_mask]
-            brown_points = cluster_points[brown_mask]
 
             # Calculate the total number of points in the cluster
             total_points = len(cluster_points)
@@ -240,31 +237,23 @@ class ExamineImage(Node):
             red_ratio = len(red_points) / total_points
             green_ratio = len(green_points) / total_points
             blue_ratio = len(blue_points) / total_points
-            brown_ratio = len(brown_points) / total_points
 
-            #self.get_logger().info(f"Brown {red_ratio} {blue_ratio} {green_ratio} {brown_ratio}")
+            #self.get_logger().info(f"Color ratios: {red_ratio} {blue_ratio} {green_ratio}")
 
-            pure_red = pure_green = pure_blue = pure_brown = False
+            pure_red = pure_green = pure_blue = False
 
             # Check if the cluster is predominantly red, green, or blue
             if red_ratio > 0.001 and green_ratio == 0.0 and blue_ratio == 0.0:
                 pure_red = True
-            elif green_ratio > 0.001 and red_ratio == 0.0 and blue_ratio == 0.0 and brown_ratio == 0.0:
+            elif green_ratio > 0.001 and red_ratio == 0.0 and blue_ratio == 0.0:
                 pure_green = True
-            elif blue_ratio > 0.001 and red_ratio == 0.0 and green_ratio == 0.0 and brown_ratio < 0.01:
+            elif blue_ratio > 0.001 and red_ratio == 0.0 and green_ratio == 0.0:
                 pure_blue = True
-            elif brown_ratio > 0.1 and red_ratio == 0.0 and green_ratio == 0.0 and blue_ratio == 0.0:
-                pure_brown = True
 
             # Classify based on floor contact points for the current cluster
             object_type = self.classify_based_on_floor_contact(cluster_points)
 
             x, y, z = np.mean(cluster_points, axis=0)
-
-            if pure_brown:
-                if object_type == "cube":
-                    #self.get_logger().info(f'🟫 Cluster {cluster_label} is a cube!')
-                    self.publish_object(x, z + 0.02, 0.0, Object.CUBE, msg.header.stamp)
 
             if pure_red or pure_green or pure_blue:
                 if object_type == "sphere":
@@ -307,10 +296,7 @@ class ExamineImage(Node):
                 self.publish_object(x + 0.01, z, 0.0, Object.PLUSHIE, msg.header.stamp)
 
             else:
-                if pure_brown:
-                    continue
-                #else:
-                    #self.get_logger().info(f'Cluster {cluster_label} is NOT a recognized object.')
+                self.get_logger().warn(f'Cluster {cluster_label} is NOT a recognized object.')
 
             # ------------ TIMER FOR EFFICIENCY CHECK (move where desired) ------------
             end_time = time.time()
@@ -534,8 +520,8 @@ class ExamineImage(Node):
         try:
             transform = self.tfBuffer.lookup_transform(
                 'map',  # Target frame
-                point_in.header.frame_id,  # Source frame
-                rclpy.time.Time(),  # Time of the transform
+                point_in.header.frame_id,  
+                point_in.header.stamp,  # Time of the transform
                 rclpy.duration.Duration(seconds=1.0)  # Timeout
             )
 
