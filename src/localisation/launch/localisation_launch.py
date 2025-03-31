d7b3c5b6781071b2da1d7514141a76871aefd999
thaeron <thaeron@kth.se>
2025-03-31 13:10:39 +0200

    updated localisation launch

--- src/localisation/launch/localisation_launch.py
+++ src/localisation/launch/localisation_launch.py
@@ -11,10 +11,25 @@ import os
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    
+    frames_launch_file = os.path.join(
+        get_package_share_directory("robp_launch"), "launch", "frames_launch.xml"
+    ) 
     return LaunchDescription([
-        
-        
+        IncludeLaunchDescription(
+            launch_description_source=AnyLaunchDescriptionSource(frames_launch_file),
+        ),
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=["0", "0.085", "0.155", "0", "0", "0", "1", "base_link", "lidar_link"],
+            output="screen",
+        ),
+        Node(
+            package="localisation",
+            executable="odometry",
+            name="robot_odometry",
+            output="screen",
+        ),
         Node(
             package="localisation",
             executable="localisation",
@@ -29,28 +44,8 @@ def generate_launch_description():
         ),
     ])
 
-""" 
 
-frames_launch_file = os.path.join(
-        get_package_share_directory("robp_launch"), "launch", "frames_launch.xml"
-    ) 
 
     
-IncludeLaunchDescription(
-            launch_description_source=AnyLaunchDescriptionSource(frames_launch_file),
-        ),
 
-Node(
-            package="tf2_ros",
-            executable="static_transform_publisher",
-            arguments=["0", "0.085", "0.155", "0", "0", "0", "1", "base_link", "lidar_link"],
-            output="screen",
-        ),
-        Node(
-            package="localisation",
-            executable="odometry",
-            name="robot_odometry",
-            output="screen",
-        ),
 
-"""
