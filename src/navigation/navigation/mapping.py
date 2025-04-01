a9df6853928f54a8503cfa3424f64129b29dd654
thaeron <thaeron@kth.se>
2025-04-01 21:47:19 +0200

    tweaks

--- src/navigation/navigation/mapping.py
+++ src/navigation/navigation/mapping.py
@@ -86,7 +86,7 @@ class Mapping(Node):
                 reading = msg.range_max
                 
 
-            if not (angle > -(math.pi * 0.15) and angle < (math.pi * 0.65)):
+            if not (angle > -(math.pi * 0.20) and angle < (math.pi * 0.70)):
                 angle += msg.angle_increment
                 continue
 
