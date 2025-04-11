2e31b6f26d49a412d4960d3e178e02073076f05a
eliaswet <eliaswet@kth.se>
2025-04-11 17:52:27 +0200

    Cut out the middleman for arm control

--- src/project_master/project_master/project_master.py
+++ src/project_master/project_master/project_master.py
@@ -61,6 +61,8 @@ class ProjectMaster(Node):
         self.pos_subscriber = self.create_subscription(
             JointState, '/servo_pos_publisher', self.pos_callback, 10)
 
+        self.joint_publisher = self.create_publisher(Int16MultiArray, "/multi_servo_cmd_sub", 10)
+
         self.base = 12000
         self.v1 = 12000
         self.v2 = 12000
@@ -122,7 +124,7 @@ class ProjectMaster(Node):
         elif target == 'box':
             closest_obj = min(boxes, key=lambda DetectedData: DetectedData.distance)
 
-        step_size = 2
+        step_size = 5
         eps = 5
 
         base = self.base
@@ -152,13 +154,17 @@ class ProjectMaster(Node):
             move_command.v1 = int(self.v1)
             move_command.v2 = int(self.v2)
             move_command.v3 = int(v3)
-            future = self.client_joint.call_async(move_command)
-            rclpy.spin_until_future_complete(self, future)
-            response = future.result()
+            
+            msg = Int16MultiArray()
+            msg.layout = MultiArrayLayout(dim=[MultiArrayDimension(label="", size=12, stride=12)], data_offset=0)
+            move_time =250
+            pose = [14000,12000,int(v3),int(self.v2),int(self.v1),int(base),move_time,move_time,move_time,move_time,move_time,move_time]
+            msg.data = pose
+            self.joint_publisher.publish(msg)
+
+            self.clock.sleep_for(rclpy.duration.Duration(seconds=0.25))
 
             objects, boxes = self.send_camera_request()
-
-            self.clock.sleep_for(rclpy.duration.Duration(seconds=0.5))
 
             if target == 'objects':
                 closest_obj = min(objects, key=lambda DetectedData: DetectedData.distance)
@@ -253,36 +259,6 @@ class ProjectMaster(Node):
 
         self.tf_broadcaster.sendTransform(t)
         self.get_logger().info(f"Published transform for {name} at ({x}, {y})")
-
-
-    # def reached_destination_callback(self, request, response):
-    #     next_x = self.end_points[self.i][0]
-    #     next_y = self.end_points[self.i][1]
-    #     next_yaw = self.end_points[self.i][2]
-    #     self.send_end_point(next_x, next_y, next_yaw)
-    #     self.i = (self.i + 1) % 4
-
-    #     response.success = True
-    #     response.message = f"Sending end point: ({next_x}, {next_y}) at {next_yaw} radians"
-    #     return response
-
-    # def send_end_point(self, x, y, yaw):
-    #     # Create new GoToPoint service for navigation node
-    #     navigation_request = GoToPoint.Request()
-    #     navigation_request.x = x
-    #     navigation_request.y = y
-    #     navigation_request.yaw = yaw
-
-    #     # Handle request and response to navigation node async
-    #     future = self.end_point_client.call_async(navigation_request)
-    #     future.add_done_callback(self.navigation_response_callback)
-
-    # def navigation_response_callback(self, future):
-    #     try:
-    #         response = future.result()
-    #         self.get_logger().info(f"Navigation response: {response.success}, {response.message}")
-    #     except Exception as e:
-    #         self.get_logger().warn(f"Service call to navigation node failed: {e}")
         
 
 
@@ -290,7 +266,7 @@ def main():
     rclpy.init()
     node = ProjectMaster()
 
-   # node.send_end_point(-1.5, 0.5)
+    # node.send_end_point(-1.5, 0.5)
 
     
     # node.process_map_file("/home/robot/project-workspace-dd2419/maps/Map_test.txt")
