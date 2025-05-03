c478f7fc13391dacc075d4a4c24a70d93fa13de8
thaeron <thaeron@kth.se>
2025-05-03 13:38:54 +0200

    behaviours in separate file

--- src/project_master/project_master/behaviours.py
+++ src/project_master/project_master/behaviours.py
@@ -2,22 +2,20 @@
 
 import math
 import numpy as np
+import time
 
 import py_trees
 import py_trees_ros
 
 import rclpy
 from rclpy.node import Node
-from tf2_ros import TransformBroadcaster
-from geometry_msgs.msg import PoseStamped, TransformStamped, Quaternion
 from rclpy.qos import QoSProfile, HistoryPolicy, ReliabilityPolicy
+from nav_msgs.msg import Odometry
 from project_interfaces.srv import GoToPoint, Trigger
-from nav_msgs.msg import Path, Odometry
 from project_interfaces.msg import Vertex, WorkspaceVertices
 from mapping.map import Map
 
 # a node has executed completely after returning a SUCCESS or FAILURE
-
 # setup - one time constructor
 # initialized - run when node was first ticked or execution completed
 # update - called every time the node is ticked
@@ -82,9 +80,6 @@ class ServiceClient(py_trees.behaviour.Behaviour):
             return py_trees.common.Status.RUNNING
 
 class ReachedEndPoint(py_trees.behaviour.Behaviour):
-    """
-    A behaviour that checks is an end point has been reached
-    """
     def __init__(self, name, x, y, yaw, distance_threshold=0.1, yaw_threshold=0.1):
         super().__init__(name)
         self.current_point = (None, None)
@@ -107,7 +102,6 @@ class ReachedEndPoint(py_trees.behaviour.Behaviour):
             reliability=ReliabilityPolicy.BEST_EFFORT
         )
 
-        self.node.waypoint_broadcaster = TransformBroadcaster(self.node)
         self.node.create_subscription(Odometry, "/odom", self.odom_callback, qos_profile)
         return True
 
@@ -117,29 +111,6 @@ class ReachedEndPoint(py_trees.behaviour.Behaviour):
         siny_cosp = 2 * (q.w * q.z + q.x * q.y)
         cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z)
         self.current_yaw = np.arctan2(siny_cosp, cosy_cosp)
-
-    def broadcast_waypoint(self):
-        quaternion = Quaternion()
-        quaternion.x = 0.0
-        quaternion.y = 0.0
-        quaternion.z = np.sin(self.target_yaw * 0.5)
-        quaternion.w = np.cos(self.target_yaw * 0.5)
-
-        transform_msg = TransformStamped()
-        transform_msg.header.frame_id = "odom"
-        transform_msg.header.stamp = self.node.get_clock().now().to_msg()
-        transform_msg.child_frame_id = "waypoint"
-
-        transform_msg.transform.translation.x = self.end_point[0]
-        transform_msg.transform.translation.y = self.end_point[1]
-        transform_msg.transform.translation.z = 0.0
-
-        transform_msg.transform.rotation.x = quaternion.x
-        transform_msg.transform.rotation.y = quaternion.y
-        transform_msg.transform.rotation.z = quaternion.z
-        transform_msg.transform.rotation.w = quaternion.w
-
-        self.node.waypoint_broadcaster.sendTransform(transform_msg)
 
     def update(self):
         if self.current_point == (None, None):
@@ -154,12 +125,44 @@ class ReachedEndPoint(py_trees.behaviour.Behaviour):
 
         if distance_error > self.distance_threshold:
             self.node.get_logger().info(f"{self.name}: Distance to waypoint is {distance_error:.2f}")
-            self.broadcast_waypoint()
             return py_trees.common.Status.RUNNING
         elif yaw_error > self.yaw_threshold:
             self.node.get_logger().info(f"{self.name}: Correcting yaw by {yaw_error:.2f}")
-            self.broadcast_waypoint()
             return py_trees.common.Status.RUNNING
         else:
             self.node.get_logger().info(f"{self.name}: Reached waypoint of ({self.end_point[0], self.end_point[1]}) at {self.current_yaw}")
             return py_trees.common.Status.SUCCESS
+
+class WaitBehavior(py_trees.behaviour.Behaviour):
+    def __init__(self, name, duration=0.5):
+        super().__init__(name)
+        self.duration = duration
+        self.start_time = None
+
+    def setup(self, **kwargs):
+        try:
+            self.node = kwargs.get("node")
+        except Exception as e:
+            self.logger.error(f"{self.name} - Setup failed: {e}")
+            return False
+        return True
+
+    def initialise(self):
+        self.start_time = time.time()
+        self.node.get_logger().info(f"{self.name}: Starting {self.duration}-second wait")
+
+    def update(self):
+        if self.start_time is None:
+            self.node.get_logger().error(f"{self.name}: Timer not initialized")
+            return py_trees.common.Status.FAILURE
+
+        elapsed = time.time() - self.start_time
+        if elapsed < self.duration:
+            return py_trees.common.Status.RUNNING
+        else:
+            self.node.get_logger().info(f"{self.name}: Wait complete")
+            return py_trees.common.Status.SUCCESS
+
+    def terminate(self, new_status=None):
+        self.start_time = None
+
--- src/project_master/project_master/explore_master.py
+++ src/project_master/project_master/explore_master.py
@@ -19,185 +19,6 @@ from project_interfaces.msg import Vertex, WorkspaceVertices
 from mapping.map import Map
 
 from project_master import behaviours
-import time 
-
-class ServiceClient(py_trees.behaviour.Behaviour):
-    def __init__(self, name, service_type, service_name, **kwargs):
-        super().__init__(name)
-        self.service_type = service_type
-        self.service_name = service_name
-        self.request_args = kwargs
-        self.client = None
-        self.future = None
-        self.sent_request = False
-
-    def setup(self, **kwargs):
-        try:
-            self.node = kwargs.get("node")
-        except Exception as e:
-            self.logger.error(f"{self.name} - Setup failed: {e}")
-            return False
-
-        self.client = self.node.create_client(self.service_type, self.service_name)
-        return True
-
-    def initialise(self):
-        self.sent_request = False
-        self.future = None
-
-    def update(self):
-        if not self.client.service_is_ready():
-            self.node.get_logger().info(f"{self.name} - Waiting for service {self.service_name} ...")
-            return py_trees.common.Status.RUNNING
-            
-        if not self.sent_request:
-            try:
-                request = self.service_type.Request()
-
-                for key, value in self.request_args.items():
-                    setattr(request, key, value)
-
-                self.future = self.client.call_async(request)
-                self.sent_request = True
-                self.node.get_logger().info(f"{self.name} - Sent request to {self.service_name}")
-                return py_trees.common.Status.RUNNING
-            except Exception as e:
-                self.node.get_logger().error(f"{self.name} - Failed to send request: {e}")
-                return py_trees.common.Status.FAILURE
-
-        if self.future.done():
-            try:
-                response = self.future.result()
-                if response.success:
-                    self.node.get_logger().info(f"{self.name} - Service call response: {response.success}, {response.message}")
-                    return py_trees.common.Status.SUCCESS
-                else:
-                    self.node.get_logger().info(f"{self.name} - Service call response: {response.success}, {response.message}")
-                    return py_trees.common.Status.FAILURE
-            except Exception as e:
-                self.node.get_logger().error(f"{self.name} - Service call failed with exception: {e}")
-                return py_trees.common.Status.FAILURE
-        else:
-            return py_trees.common.Status.RUNNING
-
-class ReachedEndPoint(py_trees.behaviour.Behaviour):
-    """
-    A behaviour that checks is an end point has been reached
-    """
-    def __init__(self, name, x, y, yaw, distance_threshold=0.1, yaw_threshold=0.1):
-        super().__init__(name)
-        self.current_point = (None, None)
-        self.current_yaw = 0.0
-        self.end_point = (x, y)
-        self.target_yaw = yaw
-        self.distance_threshold = distance_threshold
-        self.yaw_threshold = yaw_threshold
-
-    def setup(self, **kwargs):
-        try:
-            self.node = kwargs.get("node")
-        except Exception as e:
-            self.logger.error(f"{self.name} - Setup failed: {e}")
-            return False
-
-        qos_profile = QoSProfile(
-            depth=1,
-            history=HistoryPolicy.KEEP_LAST,
-            reliability=ReliabilityPolicy.BEST_EFFORT
-        )
-
-        self.node.waypoint_broadcaster = TransformBroadcaster(self.node)
-        self.node.create_subscription(Odometry, "/odom", self.odom_callback, qos_profile)
-        return True
-
-    def odom_callback(self, msg: Odometry):
-        self.current_point = (msg.pose.pose.position.x, msg.pose.pose.position.y)
-        q = msg.pose.pose.orientation
-        siny_cosp = 2 * (q.w * q.z + q.x * q.y)
-        cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z)
-        self.current_yaw = np.arctan2(siny_cosp, cosy_cosp)
-
-    def broadcast_waypoint(self):
-        quaternion = Quaternion()
-        quaternion.x = 0.0
-        quaternion.y = 0.0
-        quaternion.z = np.sin(self.target_yaw * 0.5)
-        quaternion.w = np.cos(self.target_yaw * 0.5)
-
-        transform_msg = TransformStamped()
-        transform_msg.header.frame_id = "odom"
-        transform_msg.header.stamp = self.node.get_clock().now().to_msg()
-        transform_msg.child_frame_id = "waypoint"
-
-        transform_msg.transform.translation.x = self.end_point[0]
-        transform_msg.transform.translation.y = self.end_point[1]
-        transform_msg.transform.translation.z = 0.0
-
-        transform_msg.transform.rotation.x = quaternion.x
-        transform_msg.transform.rotation.y = quaternion.y
-        transform_msg.transform.rotation.z = quaternion.z
-        transform_msg.transform.rotation.w = quaternion.w
-
-        self.node.waypoint_broadcaster.sendTransform(transform_msg)
-
-    def update(self):
-        if self.current_point == (None, None):
-            self.node.get_logger().info(f"{self.name}: Waiting for current point ...")
-            return py_trees.common.Status.RUNNING
-        
-        distance_error = np.hypot(
-            self.current_point[0] - self.end_point[0],
-            self.current_point[1] - self.end_point[1]
-        )
-        yaw_error = math.atan2(math.sin(self.target_yaw - self.current_yaw), math.cos(self.target_yaw - self.current_yaw))
-
-        if distance_error > self.distance_threshold:
-            self.node.get_logger().info(f"{self.name}: Distance to waypoint is {distance_error:.2f}")
-            self.broadcast_waypoint()
-            return py_trees.common.Status.RUNNING
-        elif yaw_error > self.yaw_threshold:
-            self.node.get_logger().info(f"{self.name}: Correcting yaw by {yaw_error:.2f}")
-            self.broadcast_waypoint()
-            return py_trees.common.Status.RUNNING
-        else:
-            self.node.get_logger().info(f"{self.name}: Reached waypoint of ({self.end_point[0], self.end_point[1]}) at {self.current_yaw}")
-            return py_trees.common.Status.SUCCESS
-    
-class WaitBehavior(py_trees.behaviour.Behaviour):
-    """
-    A behaviour that waits for a specified duration before succeeding.
-    """
-    def __init__(self, name, duration=0.5):
-        super().__init__(name)
-        self.duration = duration
-        self.start_time = None
-
-    def setup(self, **kwargs):
-        try:
-            self.node = kwargs.get("node")
-        except Exception as e:
-            self.logger.error(f"{self.name} - Setup failed: {e}")
-            return False
-        return True
-
-    def initialise(self):
-        self.start_time = time.time()
-        self.node.get_logger().info(f"{self.name}: Starting {self.duration}-second wait")
-
-    def update(self):
-        if self.start_time is None:
-            self.node.get_logger().error(f"{self.name}: Timer not initialized")
-            return py_trees.common.Status.FAILURE
-
-        elapsed = time.time() - self.start_time
-        if elapsed < self.duration:
-            return py_trees.common.Status.RUNNING
-        else:
-            self.node.get_logger().info(f"{self.name}: Wait complete")
-            return py_trees.common.Status.SUCCESS
-
-    def terminate(self, new_status=None):
-        self.start_time = None
 
 def generate_waypoints_with_map(map: Map, x_resolution, y_resolution):
 
@@ -386,7 +207,7 @@ class ExploreMaster(Node):
             transform = TransformStamped()
             transform.header.frame_id = "odom"  # Change to your desired parent frame
             transform.header.stamp = self.get_clock().now().to_msg()
-            transform.child_frame_id = f'EndPoint{i}'
+            transform.child_frame_id = f"end_point_{i}"
             
             # Set translation from end_point coordinates
             transform.transform.translation.x = point[0]
@@ -405,12 +226,12 @@ class ExploreMaster(Node):
         exploration_sequence = py_trees.composites.Sequence("Exploration", memory=True)
 
         for i, (x, y, yaw) in enumerate(self.end_points):
-            point_selector = py_trees.composites.Selector(f"EndPoint{i}", memory=True)
+            point_selector = py_trees.composites.Selector(f"EndPoint_{i}", memory=True)
 
-            service_check_sequence = py_trees.composites.Sequence(f"ServiceCheck{i}", memory=True)
+            service_check_sequence = py_trees.composites.Sequence(f"ServiceCheck_{i}", memory=True)
 
             pathing_service = behaviours.ServiceClient(
-                name=f"GoToPoint{i}",
+                name=f"GoToPoint_{i}",
                 service_type=GoToPoint,
                 service_name="/pathing_end_point",
                 x=x,
@@ -422,27 +243,27 @@ class ExploreMaster(Node):
                 approaching_object=False,
             )
 
-            retry_on_endpoint_failure = py_trees.composites.Sequence(f"RetryOnEndpointFailure{i}", memory=False)
+            retry_on_endpoint_failure = py_trees.composites.Sequence(f"RetryOnEndpointFailure_{i}", memory=False)
             
             end_point_check = behaviours.ReachedEndPoint(
-                name=f"ReachedEndPoint{i}",
+                name=f"ReachedEndPoint_{i}",
                 x=x,
                 y=y,
                 yaw=yaw,
             )
 
             retry_endpoint = py_trees.decorators.FailureIsRunning(
-                name=f"RetryEndpoint{i}",
+                name=f"RetryEndpoint_{i}",
                 child=end_point_check,
             )
 
-            wait = WaitBehavior(name=f"Wait{i}", duration=0.5)
+            wait = behaviours.WaitBehavior(name=f"Wait_{i}", duration=0.5)
 
             retry_on_endpoint_failure.add_children([pathing_service, retry_endpoint])
             #service_check_sequence.add_child(retry_on_endpoint_failure)
             service_check_sequence.add_children([retry_on_endpoint_failure, wait]) # <-- wait after endpoints
 
-            fallback = py_trees.behaviours.Success(name=f"SkipToNext{i}")
+            fallback = py_trees.behaviours.Success(name=f"SkipToNext_{i}")
 
             point_selector.add_children([service_check_sequence, fallback])
             exploration_sequence.add_child(point_selector)
