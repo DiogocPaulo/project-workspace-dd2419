52581b854ae7fdb205c17b44de2b9bc4f2e4048b
Diogo Carreira Paulo <diogop@kth.se>
2025-05-08 00:32:20 +0200

    Update object_filter_node.py

--- src/detection/detection/object_filter_node.py
+++ src/detection/detection/object_filter_node.py
@@ -116,6 +116,8 @@ class ObjectFilterNode(Node):
             
             # Find most common type if we have enough consistent observations
             if len(nearby) >= MIN_CONSISTENT:
+                mean_x = np.mean([obj.x for obj in nearby])
+                mean_y = np.mean([obj.y for obj in nearby])
                 most_common, count = max(type_counts.items(), key=lambda x: x[1])
                 if count >= MIN_CONSISTENT:
                     for i, obj in enumerate(self.object_list):
@@ -127,6 +129,8 @@ class ObjectFilterNode(Node):
                                     self.object_list[i].object_type = "box"
                                     break
                             elif dist <= CONFIDENCE_RADIUS:
+                                self.object_list[i].x = mean_x
+                                self.object_list[i].y = mean_y
                                 # Update the type in the main object list
                                 self.object_list[i].object_type = most_common
                                 break
