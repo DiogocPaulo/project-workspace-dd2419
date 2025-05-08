bcf635456e6dd812c657e56301c8451c879b2e1f
thaeron <thaeron@kth.se>
2025-05-08 15:05:06 +0200

    saving stuff

--- src/navigation/navigation/navigation.py
+++ src/navigation/navigation/navigation.py
@@ -21,7 +21,7 @@ from navigation.target_path import TargetPath
 # Robot parameters
 base = 0.3                      # Wheelbase of the vehicle
 lookahead_gain = 0.1            # Look-ahead distance gain
-lookahead_min = 0.3             # Minimum look-ahead distance
+lookahead_min = 0.2             # Minimum look-ahead distance
 distance_threshold = 0.05       # Stop distance threshold
 yaw_threshold = math.radians(5) # Stop yaw threshold
 min_velocity = 0.10             # Minimum velocity
